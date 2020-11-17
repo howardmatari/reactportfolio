71cd3361901236af751c77ed1e1618ca52577134
Matari Howard <howardmatari@gmail.com>
2020-11-16 21:58:05 -0500

    Final Review

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Footer from './components/Footer';
+import HomePage from './pages/Homepage';
+import AboutPage from './pages/Aboutpage';
+import ContactPage from './pages/Contactpage';
+
+class App extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      title: 'Matari Howard',
+      headerLinks: [
+        {title: 'Home',path:'/'},
+        {title: 'About',path:'/About'},
+        {title: 'Contact',path:'/Contact'}
+      ],
+      home: {
+        title: 'React Portfolio',
+        subtitle: 'Projects that make a difference',
+        text: 'Checkout my projects below'
+      },
+
+      about: {
+        title: 'About Me',
+        
+      },
+
+      contact: {
+        title: 'Contact',
+        
+      }
+
+    }
+  }
+  render () {
+    return (
+      <Router>
+       <Container className="p-0" fluid={true}>
+        <Navbar className="border-bottom" bg='transparent' expand='lg'>
+         <Navbar.Brand>Matari Howard</Navbar.Brand>
+  
+         <Navbar.Toggle className= 'border-0' aria-control='navbar-toggle'/>
+         <Navbar.Collapse id='navbar-toggle'>
+           <Nav className='ml-auto'>
+           <Link className='nav-link' to='/'>Home</Link>
+           <Link className='nav-link' to='/about'>About</Link>
+           <Link className='nav-link' to='/contact'>Contact</Link>
+           </Nav>
+          
+         </Navbar.Collapse>
+        
+        </Navbar>
+  
+        <Route path='/' exact render={()=> <HomePage title={this.state.home.title} subtitle={this.state.home.subtitle} text={this.state.home.text}/>}/>
+        <Route path='/about' exact render={()=> <AboutPage title={this.state.about.title}/>} />
+        <Route path='/contact' exact render={()=> <ContactPage title={this.state.contact.title}/>} />
+  
+  
+        <Footer/>
+        
+          
+        </Container>  
+      </Router>
+    );
+  }
 }
+
+
+//function App() {
+
+  
 
 export default App;
