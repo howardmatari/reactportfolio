7e8c9174bc59e483e1bba373a8594f282d064a5f
Matari Howard <howardmatari@gmail.com>
2021-08-13 13:33:51 -0400

    added br tags and changed background color

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    
+    <style>{'body { background-color: magenta; }'}</style>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,13 +8,13 @@ function HomePage(props){
        <div>
         <Hero title={props.title}/>
         <Carousel/>
-        <a href="https://docs.google.com/document/d/1CTfsIS3YwKu7rVcKIE-Rl2wuPpOZiZD1VzyAZIQbNIg/edit#" target="_blank">Resume</a>
-        <a href="https://enigmatic-journey-63404.herokuapp.com/" target="_blank">Project 1</a>
-        <a href="https://evening-badlands-50066.herokuapp.com/" target="_blank">Project 2 Gumshoe</a>
-        <a href="https://fathomless-plateau-66925.herokuapp.com/exercise?" target="_blank">Project 3 Fitness</a>
-        <a href="https://warm-dawn-68504.herokuapp.com/" target="_blank">Project 4 Burger</a>
-        <a href="https://protected-atoll-76403.herokuapp.com" target="_blank">Project 5 Notetaker</a>
-        <a href="https://modernmediamichael.github.io/firstproject/" target="_blank">Project 6</a>
+        <a href="https://docs.google.com/document/d/1CTfsIS3YwKu7rVcKIE-Rl2wuPpOZiZD1VzyAZIQbNIg/edit#" target="_blank">Resume</a><br></br>
+        <a href="https://enigmatic-journey-63404.herokuapp.com/" target="_blank">Project 1</a><br></br>
+        <a href="https://evening-badlands-50066.herokuapp.com/" target="_blank">Project 2 Gumshoe</a><br></br>
+        <a href="https://fathomless-plateau-66925.herokuapp.com/exercise?" target="_blank">Project 3 Fitness</a><br></br>
+        <a href="https://warm-dawn-68504.herokuapp.com/" target="_blank">Project 4 Burger</a><br></br>
+        <a href="https://protected-atoll-76403.herokuapp.com" target="_blank">Project 5 Notetaker</a><br></br>
+        <a href="https://modernmediamichael.github.io/firstproject/" target="_blank">Project 6</a><br></br>
         </div>
     )
 
